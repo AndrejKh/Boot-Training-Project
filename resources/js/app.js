ca4a31c4bb24bd280c2369b12068eca009461309
AndrejKh <silverwewel@gmail.com>
2023-10-27 06:10:00 -0400

    ADD: Sentry module is modified.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,14 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import * as Sentry from "@sentry/browser";
+
 window.Alpine = Alpine;
 
 Alpine.start();
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+});
+
+
